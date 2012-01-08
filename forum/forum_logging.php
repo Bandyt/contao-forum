7f76568ef398c68b2413e3a8f9e4ddb5f659a2b2
Bandyt <Bandyt@gmx.net>
2012-01-08 21:20:22 +0100

    ADDED: Forum type root ADDED: Name of root forum is used in breadcrumbs CHANGED: Forum settings are entered in forum root DELETED: DCA forum settings DELETED: BBCODE. This will come as separate extension

---
+++ forum/forum_logging.php
@@ -0,0 +1,80 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  2011 Andreas Koob 
+ * @author     Andreas Koob 
+ * @package    forum 
+ * @license    LGPL 
+ * @filesource
+ */
+
+
+/**
+ * Class forum_logging
+ *
+ * @copyright  2011 Andreas Koob 
+ * @author     Andreas Koob 
+ * @package    Controller
+ */
+class forum_logging extends frontend
+{
+	public function moderator_log($intForumId,$strAction,$intForum,$intThread,$intPost,$intUser,$intCommitter,$strChangeIP,$strChangeType,$strField,$objOldValue,$objNewValue,$strObject)
+	{
+		$functions = new forum_common_functions();
+		$intRoot=$functions->getRootFromForum($intForumId);
+		$bolLog=false;
+		switch($strAction)
+		{
+			case 'forum_logging_mod_delete_post':
+				$objSettings = $this->Database->prepare("SELECT forum_logging_mod_delete_post FROM tl_forum_forums WHERE id=?")->execute($intRootId);
+				if ($objSettings->forum_logging_mod_delete_post=='' || $objSettings->forum_logging_mod_delete_post=='L'){ $bolLog=true; }
+				break;
+		}
+		if($bolLog==true)
+		{
+			$arrSetLog = array
+			(
+				'forum' => $intForum,
+				'thread' => $intThread,
+				'post' => $intPost,
+				'user' => $intUser,
+				'committer' => $intCommitter,
+				'change_time' => time(),
+				'object' => $strObject,
+				'change_ip' => $strChangeIP,
+				'change_type' => $strChangeType,
+				'field' => $strField,
+				'old_value' => $strOldValue,
+				'new_value' => $strNewValue
+			);
+			$this->Database->prepare("INSERT INTO tl_forum_moderator_log %s")->set($arrSetLog)->execute();
+		}
+	}
+	
+	public function test()
+	{
+		$this->log("Test", 'forum_logging.test()', TL_INFO);
+	}
+}
+
+?>
